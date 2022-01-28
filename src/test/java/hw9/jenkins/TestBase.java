184c8bd350ac97eeae2ee5d9a9dada41d74bf8d9
Bogatyrev Timur <bogatyrev@lanit.ru>
2022-01-28 17:28:00 +0400

    changed pageobject test to simple

--- src/test/java/hw9/jenkins/TestBase.java
+++ src/test/java/hw9/jenkins/TestBase.java
@@ -1,20 +1,17 @@
 package hw9.jenkins;
 
 import hw9.jenkins.utils.Attach;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Configuration.*;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        addListener("allure", new AllureSelenide());
         browserSize = "1920x1080";
         baseUrl = "https://demoqa.com";
         remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
