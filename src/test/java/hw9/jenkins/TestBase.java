2042b0ce2a15d48e4e97097e4f78a9fedf5e2227
Bogatyrev Timur <bogatyrev@lanit.ru>
2022-01-28 11:22:27 +0400

    added remote hub and capabilities

--- src/test/java/hw9/jenkins/TestBase.java
+++ src/test/java/hw9/jenkins/TestBase.java
@@ -4,9 +4,9 @@ import hw9.jenkins.utils.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
-import static com.codeborne.selenide.Configuration.baseUrl;
-import static com.codeborne.selenide.Configuration.browserSize;
+import static com.codeborne.selenide.Configuration.*;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
@@ -14,9 +14,15 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+        addListener("allure", new AllureSelenide());
         browserSize = "1920x1080";
         baseUrl = "https://demoqa.com";
-        addListener("allure", new AllureSelenide());
+        remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        browserCapabilities = capabilities;
     }
 
     @AfterEach
