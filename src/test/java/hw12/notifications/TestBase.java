a71ac12888a1cfb279f6c72a9faacd13fb3344ea
Тимур <tbogatyrev@yandex.ru>
2022-02-07 00:51:41 +0400

    added notifications

---
+++ src/test/java/hw12/notifications/TestBase.java
@@ -0,0 +1,40 @@
+package hw12.notifications;
+
+import hw12.notifications.utils.Attach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Configuration.*;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        browserSize = System.getProperty("browserSize");
+        baseUrl = "https://demoqa.com";
+        remote = String.format(
+                "https://%s:%s@%s", System.getProperty("login"),
+                System.getProperty("password"),
+                System.getProperty("remoteUrl")
+        );
+        browser = System.getProperty("browser");
+        browserVersion = System.getProperty("version");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        capabilities.setCapability("name", "tbogatyrev");
+        browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+    }
+}
