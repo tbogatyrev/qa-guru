284a6b8306e42140201fbfd0842648786369f120
Тимур <tbogatyrev@yandex.ru>
2022-03-03 01:07:32 +0400

    added owner

---
+++ src/test/java/hw10/jenkins/params/config/Credentials.java
@@ -0,0 +1,8 @@
+package hw10.jenkins.params.config;
+
+import org.aeonbits.owner.ConfigFactory;
+
+public class Credentials {
+
+    public static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+}
